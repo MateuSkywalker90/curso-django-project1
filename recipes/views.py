e98fa1a5deb8b2329419a41d588482e80acb3274
Mateus Oliveira <mateuskywalker90@gmail.com>
2023-02-18 09:53:39 -0400

    Adjusting project

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,7 @@
-# from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html', context={
-        'name': 'Mateus Oliveira',
-    })
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html', context={
         'name': 'Mateus Oliveira',
     })
