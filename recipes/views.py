13e59701d9f5ba62a22b584ff8ce4a70d327794c
Mateus Oliveira <mateuskywalker90@gmail.com>
2023-02-18 18:41:37 -0400

    Creating a base html for application

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html', context={
-        'name': 'Mateus Oliveira',
-    })
+    return render(request, 'recipes/pages/home.html')
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
